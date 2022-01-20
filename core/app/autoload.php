9f2b98805bb4473ee14474756c890c6c4c2628bd
AitorMolano <56159163+AitorMolano@users.noreply.github.com>
2022-01-20 09:36:55 +0100

    Update autoload.php

--- core/app/autoload.php
+++ core/app/autoload.php
@@ -3,13 +3,13 @@
 // 10 octubre del 2014
 // esta funcion elimina el hecho de estar agregando los modelos manualmente
 
-
-function spl_autoload_register($modelname){
-	if(Model::exists($modelname)){
-		include Model::getFullPath($modelname);
-	} 
-}
-
-
+if(!function_exists('classAutoLoader')){
+        function classAutoLoader($class){
+            $class=strtolower($class);
+            $classFile=$_SERVER['DOCUMENT_ROOT'].'/include/class/'.$class.'.class.php';
+            if(is_file($classFile)&&!class_exists($class)) include $classFile;
+        }
+    }
+    spl_autoload_register('classAutoLoader');
 
 ?>
